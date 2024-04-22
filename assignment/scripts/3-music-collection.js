02c2933ec3502c01a6c229241f103b79e3a4ab06
JacobMarineau <jacobmarineaump@gmail.com>
2024-04-21 21:47:11 -0500

    Finished fourth section and created and fixed showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,27 +2,38 @@ console.log("***** Music Collection *****");
 // Safe Zone -- Write code below this line
 let myCollection = [];
 
-function addToCollection(title, artist, yearPublished, collection) {
-  let record = {
+function addToCollection(collection, title, artist, yearPublished) {
+  const record = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
   };
-  myCollection.push(record);
+  collection.push(record);
   return record;
 }
-addToCollection("Dream of the Beast", "Christina Scabbia", 2023);
-addToCollection("Swallow the Fire", "Melissa Bonny", 2023);
-addToCollection("Raw, Unfiltered Calamity", "DM DOKURO", 2018);
-addToCollection("Archangel", "Thomas Bergersen", 2011);
-addToCollection("Scarlet Hill", "Jacob Marineau", 2016);
+addToCollection(myCollection, "Dream of the Beast", "Christina Scabbia", 2023);
+addToCollection(myCollection, "Swallow the Fire", "Melissa Bonny", 2023);
+addToCollection(myCollection, "Raw, Unfiltered Calamity", "DM DOKURO", 2018);
+addToCollection(myCollection, "Archangel", "Thomas Bergersen", 2011);
+addToCollection(myCollection, "Scarlet Hill", "Jacob Marineau", 2016);
 addToCollection(
+  myCollection,
   "Chopin: Nocturne No. 1 in B-Flat Minor",
   "Frédéric Chopin",
   1830
 );
+
 console.log(myCollection);
 
+function showCollection(collection) {
+  for (let i = 0; i < collection.length; i++) {
+    const record = collection[i];
+    console.log(
+      `${record.title} by ${record.artist}, published in ${record.yearPublished}`
+    );
+  }
+}
+showCollection(myCollection);
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
 // (It's used for automated testing.)
